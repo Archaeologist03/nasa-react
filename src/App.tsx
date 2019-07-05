1840985d1ba4704e17b4d83ce053fb52327d5047
Archaeologist03 <omniiknight@hotmail.com>
2019-07-05 13:03:00 +0200

    Fixing circleci indentation. Trying out API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 import Logo from './components/Logo/Logo';
 
 const App: React.FC = () => {
+  const [nasaImg, setNasaImg] = useState('');
+
+  useEffect(() => {
+    const nasaApiKey = process.env.REACT_APP_NASA_API_KEY;
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setNasaImg(data.url);
+      });
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header' />
       <Logo />
+      <img
+        alt=''
+        src={nasaImg}
+        style={{ position: 'absolute', top: 0, left: 0 }}
+      />
     </div>
   );
 };
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Logo.module.scss';
 
 const Logo = () => {
-  const a: number = 5;
+  // const a: number = 5;
 
   return <div className={styles.logoStyle}>Hi there</div>;
 };
